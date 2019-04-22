342caeff10753fa2054899afa68e67fc9d570c60
Yefta Sutanto <ysutanto@student.unimelb.edu.au>
2019-04-22 15:52:03 +0800

    timeout need -t args

--- backend/ndis_calculator/entrypoint.sh
+++ backend/ndis_calculator/entrypoint.sh
@@ -1,4 +1,4 @@
-timeout 60 bash -c 'until echo > /dev/tcp/db/3306; do sleep 2; done'
+timeout -t 60 bash -c 'until echo > /dev/tcp/db/3306; do sleep 2; done'
 python manage.py makemigrations --no-input
 python manage.py migrate --no-input
 python manage.py runserver 0.0.0.0:8000
