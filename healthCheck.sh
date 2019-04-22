cd0332cd32289b59b55d9af7e74a14509629c696
Yefta Sutanto <ysutanto@student.unimelb.edu.au>
2019-04-22 15:42:06 +0800

    use bash tcp for healthCheck

--- healthCheck.sh
+++ healthCheck.sh
@@ -1,27 +1,30 @@
-#Sleep for 70s for django to start
-sleep 70
+#Wait for django to start
+timeout 60 bash -c 'until echo > /dev/tcp/localhost/8002; do sleep 2; done'
 
 # ping django with HTTP GET
 status_code=$(curl --write-out "%{http_code}\n" --silent --output /dev/null "http://localhost:8002/")
 #host.docker.internal = localhost of docker host
 if [ "$status_code" -ne 200 ]
 then
-    echo "Django Health check returned HTTP Response other than 200"
+    echo "Django Health Check returned HTTP Response other than 200"
     echo `curl --write-out "%{http_code}\n" --silent --output /dev/null "http://localhost:8002/"`
     echo `curl  "localhost:8002/"`
     exit 1
 else
-    echo "Django Health check returned HTTP Response 200"
+    echo "Django Health Check returned HTTP Response 200"
 fi
+
+#Wait for react to start
+timeout 60 bash -c 'until echo > /dev/tcp/localhost/3002; do sleep 2; done'
+
 # ping react with HTTP GET
 status_code_2=$(curl --write-out "%{http_code}\n" --silent --output /dev/null "http://localhost:3002/")
 if [ "$status_code_2" -ne 200 ]
 then
-    echo "React Health check returned HTTP Response other than 200"
+    echo "React Health Check returned HTTP Response other than 200"
     echo `curl --write-out "%{http_code}\n" --silent --output /dev/null "http://localhost:3002/"`
     echo `curl  "http://localhost:3002/"`
     exit 1
 else
-    echo "React Health check returned HTTP Response 200"
+    echo "React Health Check returned HTTP Response 200"
 fi
-
