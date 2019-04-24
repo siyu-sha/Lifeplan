7711b5fa0f232cf427742dee8a2a46839ff0eb7b
Ziwei Xu <zix1@student.unimelb.edu.au>
2019-04-24 20:58:51 +1000

    Initialise material-ui theme

---
+++ frontend/src/Theme.js
@@ -0,0 +1,23 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+export const CustomTheme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#173e6c"
+    },
+    secondary: {
+      main: "#97c7ef",
+      contrastText: "#173e6c"
+    }
+  },
+  typography: {
+    fontFamily: ["Montserrat", "Sans-serif"].join(",")
+  },
+  overrides: {
+    MuiButton: {
+      root: {
+        margin: 8
+      }
+    }
+  }
+});
