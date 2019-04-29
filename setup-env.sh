a16e7f899703b337735c39796bf96093e37f5238
Yefta Sutanto <ysutanto@student.unimelb.edu.au>
2019-04-29 17:54:00 +1000

    fix env vars (9)

--- setup-env.sh
+++ setup-env.sh
@@ -1,3 +1,8 @@
+# Generate random ports for Django and React
 export DJANGO_PORT=$((($RANDOM % 999) + 8000))
+echo "react-port: $REACT_PORT"
 export REACT_PORT=$((($RANDOM % 999) + 3000))
+echo "django-port: $DJANGO_PORT"
+
+# Put the port settings in an .env file for Docker
 echo -e "DOCKER_DJANGO_PORT=$DJANGO_PORT\nDOCKER_REACT_PORT=$REACT_PORT" > ".env"
