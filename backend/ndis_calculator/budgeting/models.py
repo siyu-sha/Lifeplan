66badcf4c8dc8c8d23b8d6a558705be6b402b945
Chuang Wang <chuangw@student.unimelb.edu.au>
2019-05-14 16:03:30 +1000

    add main entities

--- backend/ndis_calculator/budgeting/models.py
+++ backend/ndis_calculator/budgeting/models.py
@@ -8,7 +8,18 @@ class CustomUser(AbstractUser):
     address = models.CharField(max_length=255)
     birthday = models.DateField()
 
+class Plan(models.Model):
+    start_time = models.DateField()
+    end_time = models.DateField()
+    total_funds = models.IntegerField()
+
 class Item(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     cost = models.DecimalField(max_digits=15,decimal_places=2)
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+class Goal(models.Model):
+    description = models.TextField()
