52e510da3c0ef81bce71cb1c645104f3f218f3fa
Ziwei Xu <zix1@student.unimelb.edu.au>
2019-05-30 23:40:45 +1000

    Add close button

--- frontend/src/supports/SupportsPopup.js
+++ frontend/src/supports/SupportsPopup.js
@@ -5,6 +5,8 @@ import PropTypes from "prop-types";
 import SupportsContent from "./SupportsContent";
 import withMobileDialog from "@material-ui/core/withMobileDialog";
 import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogActions from "@material-ui/core/DialogActions";
+import Button from "@material-ui/core/Button";
 
 const stubProps = {
   category: "Assistive Technology"
@@ -16,24 +18,27 @@ class SupportsPopup extends React.Component {
     width: window.innerWidth
   };
 
-  handleClose = () => {};
+  handleClose = () => {
+    this.setState({ open: false });
+  };
 
   render() {
     const { fullScreen } = this.props;
-    const props = stubProps;
     return (
       <Grid>
         <Dialog
           fullScreen={fullScreen}
           open={this.state.open}
-          onClose={this.handleClose}
           aria-labelledby="assist-dialog-title"
           aria-describedby="assist-dialog-description"
           fullWidth={true}
           maxWidth={"sm"}
         >
-          <DialogTitle>{props.category}</DialogTitle>
+          <DialogTitle>{stubProps.category}</DialogTitle>
           <SupportsContent />
+          <DialogActions>
+            <Button onClick={this.handleClose}>Close</Button>
+          </DialogActions>
         </Dialog>
       </Grid>
     );
