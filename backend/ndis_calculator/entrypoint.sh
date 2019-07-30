c2719eede6ee505afb575159aae1c6bc6cf46c30
Lachlan Black <lachlan.black@student.unimelb.edu.au>
2019-07-30 15:16:24 +1000

    trial and error (1)

--- backend/ndis_calculator/entrypoint.sh
+++ backend/ndis_calculator/entrypoint.sh
@@ -1,4 +1,4 @@
-timeout 120 /bin/bash -c 'until echo > /dev/tcp/db/3306; do sleep 2; done' > /dev/null 2>&1 &
+timeout 120 /bin/bash -c 'until echo > /dev/tcp/db/3306; do sleep 2; done' > /dev/null 2>&1
 python manage.py makemigrations --no-input
 python manage.py migrate --no-input
 python manage.py runserver 0.0.0.0:8000
