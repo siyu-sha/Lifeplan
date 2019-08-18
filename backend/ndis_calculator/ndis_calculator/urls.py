39c6f27fdc6e6208f9e559dde7224b3819922f4e
Yifan Fan <fayf@student.unimelb.edu.au>
2019-08-18 16:52:38 +1000

    FP9001319-120: Change parameters according to the existing model

--- backend/ndis_calculator/ndis_calculator/urls.py
+++ backend/ndis_calculator/ndis_calculator/urls.py
@@ -43,7 +43,7 @@ api_patterns = [
     path('support-groups', support_group_list, name='support_group_list'),
     path('support-items', support_item_list, name='support_items_list'),
 
-    path('participants/<int:participantID>/plans/<int:planID>/plan-categories/<int:planCategoryID>/plan-items', PlanItem.create, name='plan_item_create'),
+    path('participants/<int:participantID>/plan-goals/<int:planGoalID>/plan-categories/<int:planCategoryID>/plan-items', PlanItem.create, name='plan_item_create'),
 ]
 
 urlpatterns = [
