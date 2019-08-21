70f5e8dea87dad3668926dcf89631760389401b3
lachiepb <lachlan.black@student.unimelb.edu.au>
2019-08-21 12:54:47 +1000

    Feature/backend/xx automated deployment (#67)
    * Add and configure whitenoise, swap jenkinsfile for Autodeploy on this branch for testing
    
    * correct api URL
    
    * Test (1)
    
    * Test (2)
    
    * Revert Jenkinsfile so that develop auto-deploys

--- backend/ndis_calculator/entrypoint-test.sh
+++ backend/ndis_calculator/entrypoint-test.sh
@@ -1,6 +1,7 @@
 timeout 120 /bin/bash -c 'until echo > /dev/tcp/db/3306; do sleep 2; done' > /dev/null 2>&1
 python manage.py makemigrations --no-input
 python manage.py migrate --no-input
+python manage.py collectstatic
 python manage.py loaddata budgeting/fixtures/*
 echo "Coverage RUN :"
 coverage erase
