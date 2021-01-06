65b19b4913b2ce6c6e9fd63c74b6c67076b1ec38
parixitas <sakhiya.parixita@gmail.com>
2021-01-06 18:21:47 +0530

    Worked on remove not required registeration groups from core

--- frontend/src/budget/dashboard/SupportItemDialog.js
+++ frontend/src/budget/dashboard/SupportItemDialog.js
@@ -174,7 +174,6 @@ export default function SupportItemDialog(props) {
       Promise.all([
         api.SupportItemGroups.get({ ...body, supportCategoryID: 3 }),
         api.SupportItemGroups.get({ ...body, supportCategoryID: 4 }),
-        api.SupportItemGroups.get({ ...body, supportCategoryID: 5 }),
         api.SupportItemGroups.get({ ...body, supportCategoryID: 6 }),
       ]).then((responses) => {
         _.map(responses, (response) => {
